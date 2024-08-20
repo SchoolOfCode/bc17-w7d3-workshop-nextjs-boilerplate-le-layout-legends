8be1f47352b1ebf5f2c907aa00809a59abd2d1b7
faiizow <faiizow@gmail.com>
2024-08-20 11:26:50 +0000

    error working properly now

--- app/components/Form/Form.jsx
+++ app/components/Form/Form.jsx
@@ -3,119 +3,101 @@ import { useState } from "react";
 import styles from "./Form.module.css";
 
 export default function Form() {
-    const [fullName, setFullName] = useState("");
-    const [postCode, setPostCode] = useState("");
-    const [streetAddress, setStreetAddress] = useState("");
-    const [city, setCity] = useState("");
+	const [fullName, setFullName] = useState("");
+	const [postCode, setPostCode] = useState("");
+	const [streetAddress, setStreetAddress] = useState("");
+	const [city, setCity] = useState("");
 
-    const [phoneNumber, setPhoneNumber] = useState("");
-    const [email, setEmail] = useState("");
+	const [phoneNumber, setPhoneNumber] = useState("");
+	const [email, setEmail] = useState("");
 
-    const [error, setError] = useState("");
+	const [error, setError] = useState("");
 
-    const handleName = (event) => {
-        if (event.target.value === "") {
-            setError("Please enter your name");
-            return;
-        }
-        setFullName(event.target.value);
-    };
-    const handlePostCode = (event) => {
-        if (event.target.value === "") {
-            setError("Please enter your post code");
-            return;
-        }
-        setPostCode(event.target.value);
-    };
-    const handleStreetAddress = (event) => {
-        if (event.target.value === "") {
-            setError("Please enter your street address");
-            return;
-        }
-        setStreetAddress(event.target.value);
-    };
-    const handleCity = (event) => {
-        if (event.target.value === "") {
-            setError("Please enter your city");
-            return;
-        }
-        setCity(event.target.value);
-    };
-    const handlePhoneNumber = (event) => {
-        if (event.target.value === "") {
-            setError("Please enter your phone number");
-            return;
-        }
-        setPhoneNumber(event.target.value);
-    };
-    const handleEmail = (event) => {
-        if (event.target.value === "") {
-            setError("Please enter your email address");
-            return;
-        }
-        setEmail(event.target.value);
-    };
+	const handleName = (event) => {
+		setFullName(event.target.value);
+		event.target.value.length < 3 ? setError("Please enter a valid name") : setError("");
+	};
+	const handlePostCode = (event) => {
+		setPostCode(event.target.value);
+		event.target.value.length < 6 ? setError("Please enter a valid post code") : setError("");
+	};
+	const handleStreetAddress = (event) => {
+		setStreetAddress(event.target.value);
+		event.target.value === "" ? setError("Please enter your street address") : setError("");
+	};
+	const handleCity = (event) => {
+		setCity(event.target.value);
+		event.target.value === "" ? setError("Please enter your city") : setError("");
+	};
+	const handlePhoneNumber = (event) => {
+		setPhoneNumber(event.target.value);
+		event.target.value.length < 10 ? setError("Please enter a valid phone number") : setError("");
+	};
+	const handleEmail = (event) => {
+		setEmail(event.target.value);
+		event.target.value === "" ? setError("Please enter your email address") : setError("");
+	};
 
-    return (
-        <>
-            <form>
-                <fieldset>
-                    <legend>Personal Information</legend>
-                    <label htmlFor="fullName"> Full Name </label>
-                    <input
-                        type="text"
-                        id="fullName"
-                        value={fullName}
-                        onChange={handleName}
-                    ></input>
-                    <label htmlFor="postCode"> Post Code </label>
-                    <input
-                        type="text"
-                        id="postCode"
-                        value={postCode}
-                        onChange={handlePostCode}
-                    ></input>
-                    <label htmlFor="streetAddress">
-                        {" "}
-                        House/Flat Number and Street Name{" "}
-                    </label>
-                    <input
-                        type="text"
-                        id="streetAddress"
-                        value={streetAddress}
-                        onChange={handleStreetAddress}
-                    ></input>
-                    <label htmlFor="city"> City </label>
-                    <input
-                        type="text"
-                        id="city"
-                        value={city}
-                        onChange={handleCity}
-                    ></input>
-                </fieldset>
+	return (
+		<>
+			<form>
+				<fieldset>
+					<legend>Personal Information</legend>
+					<label htmlFor="fullName"> Full Name </label>
+					<input
+						type="text"
+						id="fullName"
+						value={fullName}
+						onChange={handleName}
+					></input>
+					<label htmlFor="postCode"> Post Code </label>
+					<input
+						type="text"
+						id="postCode"
+						value={postCode}
+						onChange={handlePostCode}
+					></input>
+					<label htmlFor="streetAddress">
+						{" "}
+						House/Flat Number and Street Name{" "}
+					</label>
+					<input
+						type="text"
+						id="streetAddress"
+						value={streetAddress}
+						onChange={handleStreetAddress}
+					></input>
+					<label htmlFor="city"> City </label>
+					<input
+						type="text"
+						id="city"
+						value={city}
+						onChange={handleCity}
+					></input>
+				</fieldset>
 
-                <fieldset>
-                    <legend>Contact Information</legend>
-                    <label htmlFor="phoneNumber"> Phone Number </label>
-                    <input
-                        type="text"
-                        id="phoneNumber"
-                        value={phoneNumber}
-                        onChange={handlePhoneNumber}
-                    ></input>
-                    <label htmlFor="email"> Email Address </label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={email}
-                        onChange={handleEmail}
-                    ></input>
-                </fieldset>
-            </form>
-            {error ? <p className={styles.error} >{error}</p> : null}
+				<fieldset>
+					<legend>Contact Information</legend>
+					<label htmlFor="phoneNumber"> Phone Number </label>
+					<input
+						type="text"
+						id="phoneNumber"
+						value={phoneNumber}
+						onChange={handlePhoneNumber}
+					></input>
+					<label htmlFor="email"> Email Address </label>
+					<input
+						type="email"
+						id="email"
+						value={email}
+						onChange={handleEmail}
+					></input>
+				</fieldset>
+			</form>
+			{error ? <p className={styles.error} >{error}</p> : null}
 
-            <button type="submit" disabled={error ? true: false} >Request Design Consultation</button>
-        </>
-    );
+			<button type="submit" disabled={error ? true : false} >Request Design Consultation</button>
+		</>
+	);
 }
 
