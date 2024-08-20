b6ff508681d8c494d1592d84f57ef4e2a708f0ab
faiizow <faiizow@gmail.com>
2024-08-20 17:15:36 +0000

    styling done

--- app/components/Form/Form.jsx
+++ app/components/Form/Form.jsx
@@ -1,44 +1,54 @@
-import React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Form.module.css";
 
 export default function Form() {
+
+	// useState variables
 	const [fullName, setFullName] = useState("");
 	const [postCode, setPostCode] = useState("");
 	const [streetAddress, setStreetAddress] = useState("");
 	const [city, setCity] = useState("");
-
 	const [phoneNumber, setPhoneNumber] = useState("");
 	const [email, setEmail] = useState("");
+	const [error, setError] = useState(false);
 
-	const [error, setError] = useState("");
+	// function handlers
+
+	const validateForm = () => {
+		return fullName !== "" &&
+			postCode !== "" &&
+			streetAddress !== "" &&
+			city !== "" &&
+			phoneNumber !== "" &&
+			email !== "";
+	};
 
 	const handleName = (event) => {
 		setFullName(event.target.value);
-		event.target.value.length < 3 ? setError("Please enter a valid name") : setError("");
 	};
 	const handlePostCode = (event) => {
 		setPostCode(event.target.value);
-		event.target.value.length < 6 ? setError("Please enter a valid post code") : setError("");
 	};
+
 	const handleStreetAddress = (event) => {
 		setStreetAddress(event.target.value);
-		event.target.value === "" ? setError("Please enter your street address") : setError("");
 	};
 	const handleCity = (event) => {
 		setCity(event.target.value);
-		event.target.value === "" ? setError("Please enter your city") : setError("");
 	};
 	const handlePhoneNumber = (event) => {
 		setPhoneNumber(event.target.value);
-		event.target.value.length < 10 ? setError("Please enter a valid phone number") : setError("");
 	};
 	const handleEmail = (event) => {
 		setEmail(event.target.value);
-		event.target.value === "" ? setError("Please enter your email address") : setError("");
 	};
 
-	const handleClick = () => {
+	useEffect(() => {
+		setError(!validateForm());
+	}, [validateForm]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
 		console.dir({
 			fullName,
 			postCode,
@@ -51,20 +61,21 @@ export default function Form() {
 
 	return (
 		<>
-			<form>
-				<fieldset>
-					<legend>Personal Information</legend>
+			<div className={styles.hero}>Design Booking</div>
+			<form className={styles.form} onSubmit={handleSubmit}>
+				<legend>Personal Information</legend>
+				<fieldset className={styles.personalInfo}>
 					<label htmlFor="fullName"> Full Name </label>
 					<input
 						type="text"
-						id="fullName"
+						name="fullName"
 						value={fullName}
 						onChange={handleName}
 					></input>
 					<label htmlFor="postCode"> Post Code </label>
 					<input
 						type="text"
-						id="postCode"
+						name="postCode"
 						value={postCode}
 						onChange={handlePostCode}
 					></input>
@@ -74,40 +85,38 @@ export default function Form() {
 					</label>
 					<input
 						type="text"
-						id="streetAddress"
+						name="streetAddress"
 						value={streetAddress}
 						onChange={handleStreetAddress}
 					></input>
 					<label htmlFor="city"> City </label>
 					<input
 						type="text"
-						id="city"
+						name="city"
 						value={city}
 						onChange={handleCity}
 					></input>
 				</fieldset>
-
-				<fieldset>
-					<legend>Contact Information</legend>
+				<legend>Contact Information</legend>
+				<fieldset className={styles.contactInfo}>
 					<label htmlFor="phoneNumber"> Phone Number </label>
 					<input
-						type="text"
-						id="phoneNumber"
+						type="number"
+						name="phoneNumber"
 						value={phoneNumber}
 						onChange={handlePhoneNumber}
 					></input>
 					<label htmlFor="email"> Email Address </label>
 					<input
 						type="email"
-						id="email"
+						name="email"
 						value={email}
 						onChange={handleEmail}
 					></input>
 				</fieldset>
+				{error ? <p className={styles.error} >{error}</p> : null}
+				<button className={styles.buttonStyle} type="submit" disabled={error ? true : false} >Request Design Consultation</button>
 			</form>
-			{error ? <p className={styles.error} >{error}</p> : null}
-
-			<button onClick={handleClick} type="submit" disabled={error ? true : false} >Request Design Consultation</button>
 		</>
 	);
 }
