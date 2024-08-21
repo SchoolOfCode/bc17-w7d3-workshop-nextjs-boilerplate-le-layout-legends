33982d739993bc445673d7d33107f744f9b27f3d
faiizow <faiizow@gmail.com>
2024-08-21 13:28:19 +0000

    Loading state after submit done

--- app/components/FormReduced/FormReduced.jsx
+++ app/components/FormReduced/FormReduced.jsx
@@ -1,131 +1,150 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useReducer } from "react";
 import styles from "./FormReduced.module.css";
 
 export default function FormReduced() {
-    // useState variables
-    const [fullName, setFullName] = useState("");
-    const [postCode, setPostCode] = useState("");
-    const [streetAddress, setStreetAddress] = useState("");
-    const [city, setCity] = useState("");
-    const [phoneNumber, setPhoneNumber] = useState("");
-    const [email, setEmail] = useState("");
-    const [error, setError] = useState(false);
 
-    // function handlers
+	const [error, setError] = useState(false);
 
-    const validateForm = () => {
-        return (
-            fullName !== "" &&
-            postCode !== "" &&
-            streetAddress !== "" &&
-            city !== "" &&
-            phoneNumber !== "" &&
-            email !== ""
-        );
-    };
+	// function handlers
 
-    function handleChange(event) {
-        if (event.target.name === "fullName") {
-            setFullName(event.target.value);
-        }
-        if (event.target.name === "postCode") {
-            setPostCode(event.target.value);
-        }
-        if (event.target.name === "streetAddress") {
-            setStreetAddress(event.target.value);
-        }
-        if (event.target.name === "city") {
-            setCity(event.target.value);
-        }
-        if (event.target.name === "phoneNumber") {
-            setPhoneNumber(event.target.value);
-        }
-        if (event.target.name === "email") {
-            setEmail(event.target.value);
-        }
+	const validateForm = () => {
+		return (
+			state.fullName !== "" &&
+			state.postCode !== "" &&
+			state.streetAddress !== "" &&
+			state.city !== "" &&
+			state.phoneNumber !== "" &&
+			state.email !== ""
+		);
+	};
+
+	useEffect(() => {
+		setError(!validateForm());
+	}, [validateForm]);
+
+	const reducer = (state, action) => {
+		switch (action.type) {
+			case "FIELD_CHANGED":
+				return {
+					...state,
+					[action.payload.fieldName]: action.payload.fieldValue
+				};
+			default:
+				return state
+		};
+	};
+
+	const [state, dispatch] = useReducer(reducer, {
+		fullName: "",
+		postCode: "",
+		streetAddress: "",
+		city: "",
+		phoneNumber: "",
+		email: "",
+		loading: false,
+		showMessageActive: false,
+		showMessageType: "",
+		showMessageText: ""
+	});
+
+	function handleChange(event) {
+		dispatch({
+			type: "FIELD_CHANGED",
+			payload: {
+				fieldName: event.target.name,
+				fieldValue: event.target.value
+			}
+		});
 	}
 
-        useEffect(() => {
-            setError(!validateForm());
-        }, [validateForm]);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		dispatch({
+			type: "FIELD_CHANGED",
+			payload: {
+				fieldName: "loading",
+				fieldValue: true
+			}
+		})
+		setTimeout(() => {
+			dispatch({
+				type: "FIELD_CHANGED",
+				payload: {
+					fieldName: "showMessageActive",
+					fieldValue: true
+				}
+			})
+			console.dir(state);
+		}, 5000)
 
-        const handleSubmit = (e) => {
-            e.preventDefault();
-            console.dir({
-                fullName,
-                postCode,
-                streetAddress,
-                city,
-                phoneNumber,
-                email,
-            });
-        };
-	
-        return (
-            <>
-                <div className={styles.hero}>Design Booking</div>
-                <form className={styles.form} onSubmit={handleSubmit}>
-                    <legend>Personal Information</legend>
-                    <fieldset className={styles.personalInfo}>
-                        <label htmlFor="fullName"> Full Name </label>
-                        <input
-                            type="text"
-                            name="fullName"
-                            value={fullName}
-                            onChange={handleChange}
-                        ></input>
-                        <label htmlFor="postCode"> Post Code </label>
-                        <input
-                            type="text"
-                            name="postCode"
-                            value={postCode}
-                            onChange={handleChange}
-                        ></input>
-                        <label htmlFor="streetAddress">
-                            {" "}
-                            House/Flat Number and Street Name{" "}
-                        </label>
-                        <input
-                            type="text"
-                            name="streetAddress"
-                            value={streetAddress}
-                            onChange={handleChange}
-                        ></input>
-                        <label htmlFor="city"> City </label>
-                        <input
-                            type="text"
-                            name="city"
-                            value={city}
-                            onChange={handleChange}
-                        ></input>
-                    </fieldset>
-                    <legend>Contact Information</legend>
-                    <fieldset className={styles.contactInfo}>
-                        <label htmlFor="phoneNumber"> Phone Number </label>
-                        <input
-                            type="number"
-                            name="phoneNumber"
-                            value={phoneNumber}
-                            onChange={handleChange}
-                        ></input>
-                        <label htmlFor="email"> Email Address </label>
-                        <input
-                            type="email"
-                            name="email"
-                            value={email}
-                            onChange={handleChange}
-                        ></input>
-                    </fieldset>
-                    {error ? <p className={styles.error}>{error}</p> : null}
-                    <button
-                        className={styles.buttonStyle}
-                        type="submit"
-                        disabled={error ? true : false}
-                    >
-                        Request Design Consultation
-                    </button>
-                </form>
-            </>
-        );
-    }
+	};
+
+	return (
+		<>
+			<div className={styles.hero}>Design Booking </div>
+			<div className={`${styles.messageBox} ${state.showMessageActive ? "" : styles.hide}`}>Display something here</div>
+			<form className={`${styles.form} ${state.showMessageActive ? styles.hide : ""}`} onSubmit={handleSubmit}>
+				<legend>Personal Information</legend>
+				<fieldset className={styles.personalInfo}>
+					<label htmlFor="fullName"> Full Name </label>
+					<input
+						type="text"
+						name="fullName"
+						value={state.fullName}
+						onChange={handleChange}
+					></input>
+					<label htmlFor="postCode"> Post Code </label>
+					<input
+						type="text"
+						name="postCode"
+						value={state.postCode}
+						onChange={handleChange}
+					></input>
+					<label htmlFor="streetAddress">
+						{" "}
+						House/Flat Number and Street Name{" "}
+					</label>
+					<input
+						type="text"
+						name="streetAddress"
+						value={state.streetAddress}
+						onChange={handleChange}
+					></input>
+					<label htmlFor="city"> City </label>
+					<input
+						type="text"
+						name="city"
+						value={state.city}
+						onChange={handleChange}
+					></input>
+				</fieldset>
+				<legend>Contact Information</legend>
+				<fieldset className={styles.contactInfo}>
+					<label htmlFor="phoneNumber"> Phone Number </label>
+					<input
+						type="number"
+						name="phoneNumber"
+						value={state.phoneNumber}
+						onChange={handleChange}
+					></input>
+					<label htmlFor="email"> Email Address </label>
+					<input
+						type="email"
+						name="email"
+						value={state.email}
+						onChange={handleChange}
+					></input>
+				</fieldset>
+				{error ? <p className={styles.error}>{error}</p> : null}
+				<button
+					className={styles.buttonStyle}
+					type="submit"
+					disabled={error ? true : false}
+				>
+					{state.loading === true ? "Requesting ..." : "Request Design Consultation"}
+				</button>
+			</form>
+		</>
+	);
+}
 
