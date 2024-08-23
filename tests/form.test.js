cbe3c0ac4e4cb40548093e8073d3ea2f3daee429
Faisal Ahmed <github@fais.al>
2024-08-23 10:38:27 +0100

    tests for submit button disabled and enabled done

--- tests/form.test.js
+++ tests/form.test.js
@@ -56,3 +56,60 @@ test("check review buttons visible, turns orange when country clicked and review
   );
   await expect(reviewText).toBeVisible();
 });
+
+test("check if submit button is disabled if any fields are empty", async ({
+  page,
+}) => {
+  await page.goto("http://localhost:3000/booking");
+  const fullName = await page.getByTestId("fullName");
+
+  // Test 1, test if error shows for incorrect name
+
+  await fullName.fill("A"); // 1 character should be invalid
+  await fullName.blur();
+
+  await expect(await fullName.inputValue()).toEqual("A");
+
+  // only fullName has text, all other fields are empty
+
+  const submitButton = await page.getByTestId("button");
+
+  expect(await submitButton.getAttribute("disabled")).toBe("") // disabled attribute should exist
+
+})
+
+test("check if submit button lets you click once all fields are not empty", async ({ page }) => {
+  await page.goto("http://localhost:3000/booking");
+
+  const fullName = await page.getByTestId("fullName");
+  await fullName.fill("Arnold"); // 1 character should be invalid
+  await fullName.blur();
+  await expect(await fullName.inputValue()).toEqual("Arnold");
+
+  const postCode = await page.getByTestId("postCode");
+  await postCode.fill("SW1A 2AA");
+  await postCode.blur();
+  await expect(await postCode.inputValue()).toEqual("SW1A 2AA");
+
+  const streetAddress = await page.getByTestId("streetAddress");
+  await streetAddress.fill("123 Main Street");
+  await streetAddress.blur();
+
+  const city = await page.getByTestId("city");
+  await city.fill("London");
+  await city.blur();
+  await expect(await city.inputValue()).toEqual("London");
+
+  const phoneNumber = await page.getByTestId("phoneNumber");
+  await phoneNumber.fill("88888888888");
+  await phoneNumber.blur();
+  await expect(await phoneNumber.inputValue()).toEqual("88888888888");
+
+  const email = await page.getByTestId("email");
+  await email.fill("example@example.com");
+  await email.blur();
+  await expect(await email.inputValue()).toEqual("example@example.com");
+
+  const submitButton = await page.getByTestId("button");
+  expect(await submitButton.getAttribute("disabled")).toBe(null) // // disabled attribute should NOT exist
+});
